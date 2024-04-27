f1246073214dcc17561ab3e3555be29bf7608968
Sulaiman Ehsan <103748501+Sulaiman-Ehsan1816@users.noreply.github.com>
2024-04-27 14:55:22 +0500

    setting up a structure - v1

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,14 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AuthController;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+
+Route::get('/', [AuthController::class, 'login'])->name('login');
+
+// Route::get('/', function () {
+//     return view('login');
+// })->name("login");
