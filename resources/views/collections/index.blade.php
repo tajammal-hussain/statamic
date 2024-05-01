78629acf78bb2fcc9361934f66431d1bf8f0f7ec
Waqar_Ahmad <waqar.ahmad.brame@gmail.com>
2024-05-01 18:29:15 +0500

    fixed dropdown bugs
    this commit has fixed the minor bugs.✅

--- resources/views/collections/index.blade.php
+++ resources/views/collections/index.blade.php
@@ -8,6 +8,7 @@
 @endphp
 @extends('layouts.main')
 @section('content')
+    <script src="https://cdn.jsdelivr.net/gh/alpinejs/alpine@v2.x.x/dist/alpine.min.js" defer></script>
     <title>{{ 'Collections :: Cedar' }}</title>
     <div class="page-wrapper max-w-xl" bis_skin_checked="1">
         <div class="flex items-center justify-between mb-6" bis_skin_checked="1">
@@ -30,8 +31,30 @@
                                 </div>
                             </div>
                         </td>
-                        <td>
-                            <x-customDropdown :menuItem="$menuItems" />
+                        <td class="flex justify-center align-center text-start items-center">
+                            <div x-data="{ dropdownOpen: false }">
+                                <button @click="dropdownOpen = !dropdownOpen" class="relative z-10 block rounded-md bg-white p-2 focus:outline-none">
+                                    <svg class="w-5 h-5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 16 3">
+                                        <path d="M2 0a1.5 1.5 0 1 1 0 3 1.5 1.5 0 0 1 0-3Zm6.041 0a1.5 1.5 0 1 1 0 3 1.5 1.5 0 0 1 0-3ZM14 0a1.5 1.5 0 1 1 0 3 1.5 1.5 0 0 1 0-3Z"/>
+                                    </svg>
+                                </button>
+
+                                <div x-show="dropdownOpen" @click="dropdownOpen = false" class="fixed inset-0 h-full w-full z-10"></div>
+
+                                <div x-show="dropdownOpen" class="absolute right-0 md:right-[2rem] lg:right-96  mt-5 py-2 w-48 bg-white rounded-md shadow-xl z-20">
+                                    @foreach($menuItems as $menuItem)
+                                        @if($menuItem['label'] == 'Delete')
+                                            <a href="{{ $menuItem['route'] }}" class="block px-4 py-2 text-sm capitalize hover:text-white text-black hover:bg-red-500" style="text-align: start;">
+                                                <span class="hover:text-white"> {{ $menuItem['label'] }} </span>
+                                            </a>
+                                        @else
+                                            <a href="{{ $menuItem['route'] }}" class="block px-4 py-2 text-sm capitalize text-black hover:bg-blue-500" style="text-align: start;">
+                                                <span class="hover:text-white"> {{ $menuItem['label'] }} </span>
+                                            </a>
+                                        @endif
+                                    @endforeach
+                                </div>
+                            </div>
                         </td>
                     </tr>
                 @endforeach
--- resources/views/components/custom-dropdown.blade.php
+++ resources/views/components/custom-dropdown.blade.php
@@ -4,25 +4,27 @@
 
     <script src="https://cdn.jsdelivr.net/gh/alpinejs/alpine@v2.x.x/dist/alpine.min.js" defer></script>
 
-        <div x-data="{ dropdownOpen: true }" >
-            <button @click="dropdownOpen = !dropdownOpen" class="relative z-10 block rounded-md bg-white p-2 focus:outline-none">
-                                <svg class="w-5 h-5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 16 3">
-                                    <path d="M2 0a1.5 1.5 0 1 1 0 3 1.5 1.5 0 0 1 0-3Zm6.041 0a1.5 1.5 0 1 1 0 3 1.5 1.5 0 0 1 0-3ZM14 0a1.5 1.5 0 1 1 0 3 1.5 1.5 0 0 1 0-3Z"/>
-                                </svg>
-            </button>
+    <div x-data="{ dropdownOpen: false }">
+        <button @click="dropdownOpen = !dropdownOpen" class="relative z-10 block rounded-md bg-white p-2 focus:outline-none">
+            <svg class="w-5 h-5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 16 3">
+                <path d="M2 0a1.5 1.5 0 1 1 0 3 1.5 1.5 0 0 1 0-3Zm6.041 0a1.5 1.5 0 1 1 0 3 1.5 1.5 0 0 1 0-3ZM14 0a1.5 1.5 0 1 1 0 3 1.5 1.5 0 0 1 0-3Z"/>
+            </svg>
+        </button>
 
-            <div x-show="dropdownOpen" @click="dropdownOpen = false" class="fixed inset-0 h-full w-full z-10"></div>
+        <div x-show="dropdownOpen" @click="dropdownOpen = false" class="fixed inset-0 h-full w-full z-10"></div>
 
-            <div x-show="dropdownOpen" class="absolute right-0 mt-2 py-2 w-48 bg-white rounded-md shadow-xl z-20">
-                @foreach($menuItems as $menuItem)
-                    @if($menuItem['label'] == 'Delete')
-                <a href="{{ $menuItem['route'] }}" class="block px-4 py-2 text-sm capitalize hover:text-white text-black hover:bg-red-500" style="text-align: start; ">
-                   <span class="hover:text-white"> {{ $menuItem['label'] }} </span>
-                </a>
-                    @else
-                        <a href="{{ $menuItem['route'] }}" class="block px-4 py-2 text-sm capitalize text-black hover:bg-blue-500" style="text-align: start;">
-                            <span class="hover:text-white"> {{ $menuItem['label'] }} </span>
-                    @endif
-                @endforeach
-            </div>
+        <div x-show="dropdownOpen" class="absolute right-0 mt-2 py-2 w-48 bg-white rounded-md shadow-xl z-20">
+            @foreach($menuItems as $menuItem)
+                @if($menuItem['label'] == 'Delete')
+                    <a href="{{ $menuItem['route'] }}" class="block px-4 py-2 text-sm capitalize hover:text-white text-black hover:bg-red-500" style="text-align: start;">
+                        <span class="hover:text-white"> {{ $menuItem['label'] }} </span>
+                    </a>
+                @else
+                    <a href="{{ $menuItem['route'] }}" class="block px-4 py-2 text-sm capitalize text-black hover:bg-blue-500" style="text-align: start;">
+                        <span class="hover:text-white"> {{ $menuItem['label'] }} </span>
+                    </a>
+                @endif
+            @endforeach
         </div>
+    </div>
+
